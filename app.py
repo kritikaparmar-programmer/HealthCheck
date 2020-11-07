14ce9fcf3b123ddecd91e9c870aecab51b339174
Kritika Parmar <59971890+kritikaparmar-programmer@users.noreply.github.com>
2020-11-07 23:26:17 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,118 @@
+# Importing essential libraries
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+import joblib
+
+app = Flask(__name__)
+
+# Load the Random Forest CLassifier model
+filename = 'models/diabetes-prediction-rfc-model.pkl'
+filename1 = 'models/cancer-model.pkl'
+classifier = pickle.load(open(filename, 'rb'))
+rf = pickle.load(open(filename1, 'rb'))
+
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/diabetes')
+def diabetes():
+    return render_template('diabetes.html')
+
+@app.route('/predict_diabetes', methods=['POST'])
+def predict_diabetes():
+    if request.method == 'POST':
+        preg = int(request.form['pregnancies'])
+        glucose = int(request.form['glucose'])
+        bp = int(request.form['bloodpressure'])
+        st = int(request.form['skinthickness'])
+        insulin = int(request.form['insulin'])
+        bmi = float(request.form['bmi'])
+        dpf = float(request.form['dpf'])
+        age = int(request.form['age'])
+        
+        data = np.array([[preg, glucose, bp, st, insulin, bmi, dpf, age]])
+        my_prediction = classifier.predict(data)
+        
+        return render_template('d_result.html', prediction=my_prediction)
+
+
+@app.route('/cancer')
+def cancer():
+    return render_template('cancer.html')
+
+@app.route('/predict_cancer', methods=['POST'])
+def predict_cancer():
+    if request.method == 'POST':
+        rad = float(request.form['Radius_mean'])
+        tex = float(request.form['Texture_mean'])
+        par = float(request.form['Perimeter_mean'])
+        area = float(request.form['Area_mean'])
+        smooth = float(request.form['Smoothness_mean'])
+        compact = float(request.form['Compactness_mean'])
+        con = float(request.form['Concavity_mean'])
+        concave = float(request.form['concave points_mean'])
+        sym = float(request.form['symmetry_mean'])
+        frac = float(request.form['fractal_dimension_mean'])
+        rad_se = float(request.form['radius_se'])
+        tex_se = float(request.form['texture_se'])
+        par_se = float(request.form['perimeter_se'])
+        area_se = float(request.form['area_se'])
+        smooth_se = float(request.form['smoothness_se'])
+        compact_se = float(request.form['compactness_se'])
+        con_se = float(request.form['concavity_se'])
+        concave_se = float(request.form['concave points_se'])
+        sym_se = float(request.form['symmetry_se'])
+        frac_se = float(request.form['fractal_dimension_se'])
+        rad_worst = float(request.form['radius_worst'])
+        tex_worst = float(request.form['texture_worst'])
+        par_worst = float(request.form['perimeter_worst'])
+        area_worst = float(request.form['area_worst'])
+        smooth_worst = float(request.form['smoothness_worst'])
+        compact_worst = float(request.form['compactness_worst'])
+        con_worst = float(request.form['concavity_worst'])
+        concave_worst = float(request.form['concave points_worst'])
+        sym_worst = float(request.form['symmetry_worst'])
+        frac_worst = float(request.form['fractal_dimension_worst'])
+
+        data = np.array([[rad, tex, par, area, smooth, compact, con, concave, sym, frac, rad_se, tex_se, par_se, area_se, smooth_se, compact_se, con_se, concave_se, sym_se, frac_se, rad_worst, tex_worst, par_worst, area_worst, smooth_worst, compact_worst, con_worst, concave_worst, sym_worst, frac_worst]])
+        my_prediction = rf.predict(data)
+        
+        return render_template('c_result.html', prediction=my_prediction)
+
+
+def ValuePredictor(to_predict_list, size):
+    loaded_model = joblib.load('models/heart_model')
+    to_predict = np.array(to_predict_list).reshape(1,size)
+    result = loaded_model.predict(to_predict)
+    return result[0]
+
+
+@app.route('/heart')
+def heart():
+    return render_template('heart.html')
+
+@app.route('/predict_heart', methods=['POST'])
+def predict_heart():
+    
+    if request.method == 'POST':
+        to_predict_list = request.form.to_dict()
+        to_predict_list=list(to_predict_list.values())
+        to_predict_list = list(map(float, to_predict_list))
+        result = ValuePredictor(to_predict_list,11)
+
+    if(int(result)==1):
+        prediction=1
+    else:
+        prediction=0 
+   
+    return render_template('h_result.html', prediction=prediction)
+
+if __name__ == '__main__':
+	app.run(debug=True)
