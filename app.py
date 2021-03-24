8b0d87a4bdb1bfcbd82e13369b03f9f55468d823
barnamensch056 <barnadipdey2510@gmail.com>
2021-03-25 01:19:00 +0530

    models are ready

--- app.py
+++ app.py
@@ -15,15 +15,18 @@ rf = pickle.load(open(filename1, 'rb'))
 
 @app.route('/')
 def home():
-	return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/about')
 def about():
-	return render_template('about.html')
+    return render_template('about.html')
+
 
 @app.route('/diabetes')
 def diabetes():
     return render_template('diabetes.html')
+
 
 @app.route('/predict_diabetes', methods=['POST'])
 def predict_diabetes():
@@ -36,16 +39,17 @@ def predict_diabetes():
         bmi = float(request.form['bmi'])
         dpf = float(request.form['dpf'])
         age = int(request.form['age'])
-        
+
         data = np.array([[preg, glucose, bp, st, insulin, bmi, dpf, age]])
         my_prediction = classifier.predict(data)
-        
+
         return render_template('d_result.html', prediction=my_prediction)
 
 
 @app.route('/cancer')
 def cancer():
     return render_template('cancer.html')
+
 
 @app.route('/predict_cancer', methods=['POST'])
 def predict_cancer():
@@ -81,15 +85,16 @@ def predict_cancer():
         sym_worst = float(request.form['symmetry_worst'])
         frac_worst = float(request.form['fractal_dimension_worst'])
 
-        data = np.array([[rad, tex, par, area, smooth, compact, con, concave, sym, frac, rad_se, tex_se, par_se, area_se, smooth_se, compact_se, con_se, concave_se, sym_se, frac_se, rad_worst, tex_worst, par_worst, area_worst, smooth_worst, compact_worst, con_worst, concave_worst, sym_worst, frac_worst]])
+        data = np.array([[rad, tex, par, area, smooth, compact, con, concave, sym, frac, rad_se, tex_se, par_se, area_se, smooth_se, compact_se, con_se, concave_se,
+                          sym_se, frac_se, rad_worst, tex_worst, par_worst, area_worst, smooth_worst, compact_worst, con_worst, concave_worst, sym_worst, frac_worst]])
         my_prediction = rf.predict(data)
-        
+
         return render_template('c_result.html', prediction=my_prediction)
 
 
 def ValuePredictor(to_predict_list, size):
     loaded_model = joblib.load('models/heart_model')
-    to_predict = np.array(to_predict_list).reshape(1,size)
+    to_predict = np.array(to_predict_list).reshape(1, size)
     result = loaded_model.predict(to_predict)
     return result[0]
 
@@ -98,21 +103,23 @@ def ValuePredictor(to_predict_list, size):
 def heart():
     return render_template('heart.html')
 
+
 @app.route('/predict_heart', methods=['POST'])
 def predict_heart():
-    
+
     if request.method == 'POST':
         to_predict_list = request.form.to_dict()
-        to_predict_list=list(to_predict_list.values())
+        to_predict_list = list(to_predict_list.values())
         to_predict_list = list(map(float, to_predict_list))
-        result = ValuePredictor(to_predict_list,11)
+        result = ValuePredictor(to_predict_list, 11)
 
-    if(int(result)==1):
-        prediction=1
+    if(int(result) == 1):
+        prediction = 1
     else:
-        prediction=0 
-   
+        prediction = 0
+
     return render_template('h_result.html', prediction=prediction)
 
+
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
