7a4bd66f990367c9dda7963fd447a72d970f4956
Satyam Mishra <sm6003830@gmail.com>
2023-11-01 22:10:51 +0530

    Rosp Project Done

--- HealthCheck_App/android/app/src/main/kotlin/com/example/HealthCheck_App/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.HealthCheck_App
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- app.py
+++ app.py
@@ -12,9 +12,7 @@ app = Flask(__name__)
 app.secret_key = 'O.\x89\xcc\xa0>\x96\xf7\x871\xa2\xe6\x9a\xe4\x14\x91\x0e\xe5)\xd9'
 
 # Load the Random Forest CLassifier model
-filename = 'Models/diabetes-model.pkl'
 filename1 = 'Models/cancer-model.pkl'
-classifier = pickle.load(open(filename, 'rb'))
 rf = pickle.load(open(filename1, 'rb'))
 
 
@@ -26,35 +24,6 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
-
-
-@app.route('/diabetes')
-def diabetes():
-    return render_template('diabetes.html')
-
-
-@app.route('/predict_diabetes', methods=['POST'])
-def predict_diabetes():
-    if request.method == 'POST':
-        try:
-            preg = int(request.form['pregnancies'])
-            glucose = int(request.form['glucose'])
-            bp = int(request.form['bloodpressure'])
-            st = int(request.form['skinthickness'])
-            insulin = int(request.form['insulin'])
-            bmi = float(request.form['bmi'])
-            dpf = float(request.form['dpf'])
-            age = int(request.form['age'])
-
-            data = np.array([[preg, glucose, bp, st, insulin, bmi, dpf, age]])
-            my_prediction = classifier.predict(data)
-
-            return render_template('d_result.html', prediction=my_prediction)
-        except ValueError:
-            flash(
-                'Invalid input. Please fill in the form with appropriate values', 'info')
-            return redirect(url_for('diabetes'))
-
 
 @app.route('/cancer')
 def cancer():
@@ -101,171 +70,10 @@ def predict_cancer():
 
         return render_template('c_result.html', prediction=my_prediction)
 
-
-def ValuePredictor(to_predict_list, size):
-    loaded_model = joblib.load('models/heart_model')
-    to_predict = np.array(to_predict_list).reshape(1, size)
-    result = loaded_model.predict(to_predict)
-    return result[0]
-
-
-@app.route('/heart')
-def heart():
-    return render_template('heart.html')
-
-
-@app.route('/predict_heart', methods=['POST'])
-def predict_heart():
-
-    if request.method == 'POST':
-        try:
-            to_predict_list = request.form.to_dict()
-            to_predict_list = list(to_predict_list.values())
-            to_predict_list = list(map(float, to_predict_list))
-            result = ValuePredictor(to_predict_list, 11)
-
-            if(int(result) == 1):
-                prediction = 1
-            else:
-                prediction = 0
-
-            return render_template('h_result.html', prediction=prediction)
-        except ValueError:
-            flash(
-                'Invalid input. Please fill in the form with appropriate values', 'info')
-            return redirect(url_for('heart'))
-
-
-# this function use to predict the output for Fetal Health from given data
-def fetal_health_value_predictor(data):
-    try:
-        # after get the data from html form then we collect the values and
-        # converts into 2D numpy array for prediction
-        data = list(data.values())
-        data = list(map(float, data))
-        data = np.array(data).reshape(1, -1)
-        # load the saved pre-trained model for new prediction
-        model_path = 'Models/fetal-health-model.pkl'
-        model = pickle.load(open(model_path, 'rb'))
-        result = model.predict(data)
-        result = int(result[0])
-        status = True
-        # returns the predicted output value
-        return (result, status)
-    except Exception as e:
-        result = str(e)
-        status = False
-        return (result, status)
-
-
-# this route for prediction of Fetal Health
-@app.route('/fetal_health', methods=['GET', 'POST'])
-def fetal_health_prediction():
-    if request.method == 'POST':
-        # geting the form data by POST method
-        data = request.form.to_dict()
-        # passing form data to castome predict method to get the result
-        result, status = fetal_health_value_predictor(data)
-        if status:
-            # if prediction happens successfully status=True and then pass uotput to html page
-            return render_template('fetal_health.html', result=result)
-        else:
-            # if any error occured during prediction then the error msg will be displayed
-            return f'<h2>Error : {result}</h2>'
-
-    # if the user send a GET request to '/fetal_health' route then we just render the html page
-    # which contains a form for prediction
-    return render_template('fetal_health.html', result=None)
-
-
-def strokeValuePredictor(s_predict_list):
-    '''function to predict the output by data we get
-    from the route'''
-
-    model = joblib.load('Models/stroke_model.pkl')
-    data = np.array(s_predict_list).reshape(1, -1)
-    result = model.predict(data)
-    return result[0]
-
-
-@app.route('/stroke')
-def stroke():
-    return render_template('stroke.html')
-
-
-@app.route('/predict_stroke', methods=['POST'])
-# this route for predicting chances of stroke
-def predict_stroke():
-
-    if request.method == 'POST':
-        s_predict_list = request.form.to_dict()
-        s_predict_list = list(s_predict_list.values())
-        # list to keep the values of the dictionary items of request.form field
-        s_predict_list = list(map(float, s_predict_list))
-        result = strokeValuePredictor(s_predict_list)
-
-        if(int(result) == 1):
-            prediction = 1
-        else:
-            prediction = 0
-        return render_template('st_result.html', prediction=prediction)
-
-
-def liverprediction(final_features):
-    # Loading the pickle file
-    model_path = 'Models/liver-disease_model.pkl'
-    model = pickle.load(open(model_path, 'rb'))
-    result = model.predict(final_features)
-    return result[0]
-
-
-@app.route('/liver')
-def liver():
-    return render_template('liver.html')
-
-
-@app.route('/predict_liver', methods=['POST'])
-# predicting
-def predict_liver_disease():
-
-    if request.method == 'POST':
-        int_features = [float(x) for x in request.form.values()]
-        final_features = [np.array(int_features)]
-        output = liverprediction(final_features)
-        pred = int(output)
-
-        return render_template('liver_result.html', prediction=pred)
-
-
-@app.route("/malaria", methods=['GET', 'POST'])
-def malaria():
-    return render_template('malaria.html')
-
-
-@app.route("/malariapredict", methods=['POST', 'GET'])
-def malariapredict():
-    if request.method == 'POST':
-        try:
-            if 'image' in request.files:
-                img = Image.open(request.files['image'])
-                img = img.resize((50, 50))
-                img = np.asarray(img)
-                img = img.reshape((1, 50, 50, 3))
-                img = img.astype(np.float64)
-
-                model_path = "Models/malaria-model.h5"
-                model = tf.keras.models.load_model(model_path)
-                pred = np.argmax(model.predict(img)[0])
-        except:
-            message = "Please upload an Image"
-            return render_template('malaria.html', message=message)
-    return render_template('malaria_predict.html', pred=pred)
-
-
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)        
